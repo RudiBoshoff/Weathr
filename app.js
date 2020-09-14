bed6bc6540e46f6b85234414bc76b958e34d1705
Rudi Boshoff <rudiboshoff15@gmail.com>
2020-09-14 14:22:18 +0200

    Implement OOP JS and improve readability

--- app.js
+++ app.js
@@ -1,245 +1,275 @@
-window.addEventListener("load", () => {
-  // set default location to lynnwood
-  let lat = -25.7643;
-  let long = 28.2673;
-  let typingTimer;
-  let doneTypingInterval = 3500; //time in ms (5 seconds)
-  const apiKey = "852564f7ef87252587bbbb9057379b0d";
-  let apiPosition = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
-  let apiCity;
+class Time {
+  getTime() {
+    const today = new Date();
+    const [hours, minutes, seconds] = [
+      this.formatTime(today.getHours()),
+      this.formatTime(today.getMinutes()),
+      this.formatTime(today.getSeconds()),
+    ];
+    return `${hours}:${minutes}:${seconds}`;
+  }
+  setTime() {
+    const time = document.querySelector(".js-time");
+    setInterval(() => {
+      time.textContent = this.getTime();
+    }, 1000);
+  }
+  formatTime(t) {
+    return t < 10 ? `0${t}` : t;
+  }
+  convertUnixTime(t) {
+    return new Date(t * 1e3).toISOString().slice(-13, -8);
+  }
+}
 
-  const elTemp = document.querySelector(".js-temperature");
-  const elTempMax = document.querySelector(".js-temperature-max");
-  const elTempMin = document.querySelector(".js-temperature-min");
-  const elDesc = document.querySelector(".js-description");
-  const elLoc = document.querySelector(".js-location");
-  const elUnit = document.querySelector(".js-temperature-unit");
-  const elClick = document.querySelector(".js-temperature-toggle");
-  const toggle = document.querySelector(".js-search");
-  const elSearch = document.querySelector(".search");
-  // details
-  const elRealFeel = document.querySelector(".js-realfeel");
-  const elWind = document.querySelector(".js-wind");
-  const elHumidity = document.querySelector(".js-humidity");
-  const elSunrise = document.querySelector(".js-sunrise");
-  const elSunset = document.querySelector(".js-sunset");
-  const elPressure = document.querySelector(".js-pressure");
-
-  initialize();
-
-  function initialize() {
-    setTime();
-    fetchAPI(apiPosition);
-    toggleUnits();
-    checkGeolocation();
+class Weather {
+  constructor() {
+    this.temperature;
+    this.min;
+    this.max;
+    this.lat = 51.5074;
+    this.long = -0.1278;
+    this.real_feel;
+    this.humidity;
+    this.pressure;
+    this.description;
+    this.icon;
+    this.icon_ID;
+    this.name;
+    this.wind_speed;
+    this.sunrise;
+    this.sunset;
+    this.units = "metric";
+    this.query;
+    this.timezone;
   }
 
-  function toggleUnits() {
-    //  Change temperature to Celcius/Fehrenheit
-    elClick.addEventListener("click", () => {
-      changeUnit();
-    });
+  setValues(data) {
+    this.temperature = Math.floor(data.main.temp);
+    this.min = Math.floor(data.main.temp_min);
+    this.max = Math.floor(data.main.temp_max);
+    this.pressure = data.main.pressure;
+    this.humidity = data.main.humidity;
+    this.real_feel = Math.floor(data.main.feels_like);
+    this.name = data.name;
+    this.wind_speed = data.wind.speed;
+    this.sunrise = time.convertUnixTime(data.sys.sunrise + data.timezone);
+    this.sunset = time.convertUnixTime(data.sys.sunset + data.timezone);
+    this.description = data.weather[0].description;
+    this.icon_ID = data.weather[0].icon;
+    this.timezone = data.timezone;
   }
 
-  function checkGeolocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        //   retrieve user location
-        getLocation(position);
-        apiPosition = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
-        fetchAPI(apiPosition);
-      });
-    }
-  }
-
-  function changeUnit() {
-    if (elUnit.textContent === "°C") {
-      elUnit.textContent = "°F";
-      elTemp.textContent = Math.round(fehreinheit(elTemp.innerHTML));
-      elTempMax.textContent = Math.round(fehreinheit(elTempMax.innerHTML));
-      elTempMin.textContent = Math.round(fehreinheit(elTempMin.innerHTML));
-      elRealFeel.textContent = Math.round(fehreinheit(elRealFeel.innerHTML));
-    } else {
-      elUnit.textContent = "°C";
-      elTemp.textContent = Math.round(celcius(elTemp.innerHTML));
-      elTempMax.textContent = Math.round(celcius(elTempMax.innerHTML));
-      elTempMin.textContent = Math.round(celcius(elTempMin.innerHTML));
-      elRealFeel.textContent = Math.round(celcius(elRealFeel.innerHTML));
-    }
-  }
-
-  function setElements(data) {
-    const {
-      temp_min,
-      temp_max,
-      temp,
-      feels_like,
-      humidity,
-      pressure,
-    } = data.main;
-    const { name } = data;
-    const { speed } = data.wind;
-    const { sunrise, sunset } = data.sys;
-    const { description, icon } = data.weather[0];
-
-    //   SET DOM Elements from API
-    elLoc.textContent = name;
-    elDesc.textContent = description;
-    elTemp.textContent = Math.floor(temp);
-    elTempMax.textContent = Math.floor(temp_max);
-    elTempMin.textContent = Math.floor(temp_min);
-
-    // details
-    elRealFeel.textContent = Math.floor(feels_like);
-    elWind.textContent = speed;
-    elHumidity.textContent = humidity + " %";
-    elSunrise.textContent = convertTime(sunrise);
-    elSunset.textContent = convertTime(sunset);
-    elPressure.textContent = pressure + " pa";
-    return icon;
-  }
-
-  function convertTime(time) {
-    let timeToFormat = new Date(time * 1000);
-    let hours = checkTime(timeToFormat.getHours());
-    let minutes = checkTime(timeToFormat.getMinutes());
-    return `${hours}:${minutes}`;
-  }
-
-  function fetchAPI(api) {
-    // open weather api free plan
-    fetch(api)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        if (data != undefined) {
-          console.log(data);
-          icon = setElements(data);
-          setIcons(icon, document.querySelector(".js-icon"));
-        }
-      });
-  }
-
-  function getLocation(position) {
-    lat = position.coords.latitude;
-    long = position.coords.longitude;
-  }
-
-  function setIcons(icon, iconID) {
-    // links icons from open weather api to skycons
+  setIcon() {
     const skycons = new Skycons({ color: "white" });
-    let currentIcon;
+    this.getIcon();
+    skycons.play();
+    skycons.set(document.querySelector(".js-icon"), Skycons[weather.icon]);
+    // return skycons.set(iconID, Skycons.FOG);
+  }
 
-    switch (icon) {
+  getIcon() {
+    switch (this.icon_ID) {
       // clear
       case "01d":
-        currentIcon = "CLEAR_DAY";
+        this.icon = "CLEAR_DAY";
         break;
       case "01n":
-        currentIcon = "CLEAR_NIGHT";
+        this.icon = "CLEAR_NIGHT";
         break;
 
       // few clouds
       case "02d":
-        currentIcon = "PARTLY_CLOUDY_DAY";
+        this.icon = "PARTLY_CLOUDY_DAY";
         break;
       case "02n":
-        currentIcon = "PARTLY_CLOUDY_NIGHT";
+        this.icon = "PARTLY_CLOUDY_NIGHT";
         break;
 
       // scattered clouds
       case "03d":
-        currentIcon = "CLOUDY";
+        this.icon = "CLOUDY";
         break;
       case "03n":
-        currentIcon = "CLOUDY";
+        this.icon = "CLOUDY";
         break;
 
       // broken clouds
       case "04d":
-        currentIcon = "CLOUDY";
+        this.icon = "CLOUDY";
         break;
       case "04n":
-        currentIcon = "CLOUDY";
+        this.icon = "CLOUDY";
         break;
 
       // shower rain
       case "09d":
-        currentIcon = "RAIN";
+        this.icon = "RAIN";
         break;
       case "09n":
-        currentIcon = "RAIN";
+        this.icon = "RAIN";
         break;
 
       // rain
       case "10d":
-        currentIcon = "RAIN";
+        this.icon = "RAIN";
         break;
       case "10n":
-        currentIcon = "RAIN";
+        this.icon = "RAIN";
         break;
 
       // thunderstorm
       case "11d":
-        currentIcon = "RAIN";
+        this.icon = "RAIN";
         break;
       case "11n":
-        currentIcon = "RAIN";
+        this.icon = "RAIN";
         break;
 
       // snow
       case "13d":
-        currentIcon = "SNOW";
+        this.icon = "SNOW";
         break;
       case "13n":
-        currentIcon = "SNOW";
+        this.icon = "SNOW";
         break;
 
       // mist/fog
       case "50d":
-        currentIcon = "FOG";
+        this.icon = "FOG";
         break;
       case "50n":
-        currentIcon = "FOG";
+        this.icon = "FOG";
         break;
     }
-    skycons.play();
-    return skycons.set(iconID, Skycons[currentIcon]);
-    // return skycons.set(iconID, Skycons.FOG);
+  }
+}
+
+class API {
+  constructor() {
+    this.key = "852564f7ef87252587bbbb9057379b0d";
+    this.address;
+    this.data;
   }
 
-  function fehreinheit(temp) {
-    return temp * (9 / 5) + 32;
+  setApiAddress() {
+    if (weather.query) {
+      this.address = `https://api.openweathermap.org/data/2.5/weather?q=${weather.query}&appid=${this.key}&units=${weather.units}`;
+    } else if (weather.lat && weather.long) {
+      this.address = `https://api.openweathermap.org/data/2.5/weather?lat=${weather.lat}&lon=${weather.long}&appid=${this.key}&units=${weather.units}`;
+    }
   }
 
-  function celcius(temp) {
-    return ((temp - 32) * 5) / 9;
+  fetchApi() {
+    fetch(this.address)
+      .then((response) => {
+        return response.json();
+      })
+      .then((serverData) => {
+        if (serverData != undefined) {
+          this.data = serverData;
+          console.log(api);
+          weather.setValues(this.data);
+          weather.setIcon();
+          displayValues();
+        }
+      });
+  }
+}
+
+function displayValues() {
+  //main section
+  const temperature = document.querySelector(".js-temperature");
+  const max = document.querySelector(".js-temperature-max");
+  const min = document.querySelector(".js-temperature-min");
+  const description = document.querySelector(".js-description");
+  const location = document.querySelector(".js-location");
+  const unit = document.querySelector(".js-temperature-unit");
+
+  // details section
+  const realFeel = document.querySelector(".js-realfeel");
+  const windSpeed = document.querySelector(".js-wind");
+  const humidity = document.querySelector(".js-humidity");
+  const sunrise = document.querySelector(".js-sunrise");
+  const sunset = document.querySelector(".js-sunset");
+  const pressure = document.querySelector(".js-pressure");
+
+  // set values
+  //   with units
+  if (weather.units == "metric") {
+    unit.textContent = "°C";
+    windSpeed.textContent = `${weather.wind_speed} m/s`;
+  } else {
+    unit.textContent = "°F";
+    windSpeed.textContent = `${weather.wind_speed} mph`;
   }
 
-  function checkTime(i) {
-    // ensure single digit times are accompanied by a zero
-    return i < 10 ? "0" + i : i;
-  }
+  //   without units
+  location.textContent = weather.name;
+  description.textContent = weather.description;
+  temperature.textContent = weather.temperature;
+  realFeel.textContent = `${weather.real_feel} °`;
+  humidity.textContent = `${weather.humidity} %`;
+  max.textContent = weather.max;
+  min.textContent = weather.min;
+  sunrise.textContent = weather.sunrise;
+  sunset.textContent = weather.sunset;
+  pressure.textContent = `${weather.pressure} hPa`;
+}
 
-  function setTime() {
-    // live time update
-    let elTime = document.querySelector(".js-time");
-    let today = new Date();
-    let h = checkTime(today.getHours());
-    let m = checkTime(today.getMinutes());
-    // let s = checkTime(today.getSeconds());
-    // elTime.textContent = `${h}:${m}:${s}`;
-    elTime.textContent = `${h}:${m}`;
-    setTimeout(function () {
-      setTime();
-    }, 500);
+function toggleUnits() {
+  if (weather.units == "metric") {
+    weather.units = "imperial";
+  } else {
+    weather.units = "metric";
   }
+}
 
-  toggle.addEventListener("click", () => {
+function setUserPosition() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    weather.lat = position.coords.latitude;
+    weather.long = position.coords.longitude;
+    api.setApiAddress();
+    api.fetchApi();
+    displayValues();
+  });
+}
+
+function checkGeolocation() {
+  if (navigator.geolocation) {
+    setUserPosition();
+  }
+}
+
+function initialize() {
+  time.setTime();
+  checkGeolocation();
+  api.setApiAddress();
+  api.fetchApi();
+  displayValues();
+}
+
+const time = new Time();
+const weather = new Weather();
+const api = new API();
+
+window.addEventListener("DOMContentLoaded", () => {
+  initialize();
+
+  const unitToggle = document.querySelector(".js-temperature-toggle");
+  const searchToggle = document.querySelector(".js-search");
+  const search = document.querySelector(".search");
+
+  unitToggle.addEventListener("click", () => {
+    toggleUnits();
+    api.setApiAddress();
+    api.fetchApi();
+    displayValues();
+  });
+
+  searchToggle.addEventListener("click", () => {
     // search button animation
-    const search = document.querySelector(".search");
-    toggle.classList.toggle("ripple");
+    searchToggle.classList.toggle("ripple");
     search.classList.toggle("grow");
     if (search.classList.contains("grow")) {
       search.focus();
@@ -248,25 +278,30 @@ window.addEventListener("load", () => {
     }
   });
 
-  elSearch.addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-      findCity();
-    }
-    clearTimeout(typingTimer);
-    if (elSearch.value) {
-      typingTimer = setTimeout(doneTyping, doneTypingInterval);
+  search.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+      weather.query = search.value;
+      api.setApiAddress();
+      api.fetchApi();
+      displayValues();
+      search.value = "";
     }
   });
-
-  function findCity() {
-    let query = elSearch.value;
-    apiCity = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=metric`;
-    fetchAPI(apiCity);
-    elSearch.value = "";
-  }
-
-  //user is "finished typing," do something
-  function doneTyping() {
-    findCity();
-  }
 });
+
+//
+
+// const getPosition = function () {
+//     return new Promise(function (resolve, reject) {
+//         navigator.geolocation.getCurrentPosition(resolve, reject);
+//     });
+// };
+
+// getPosition()
+//     .then((position) => {
+//         weather.lat = position.coords.latitude;
+//         weather.long = position.coords.longitude;
+//     })
+//     .catch((err) => {
+//         console.error(err.message);
+//     });
